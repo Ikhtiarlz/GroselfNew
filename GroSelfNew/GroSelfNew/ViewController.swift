b1eaec835a96b16c6f105ebfbeca4b14f190d9dc
semalamTerbang <71621452+Ikhtiarlz@users.noreply.github.com>
2024-05-15 16:51:56 +0800

    GroselfNew Project

---
+++ GroSelfNew/GroSelfNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroSelfNew
+//
+//  Created by STDC_09 on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
